efd81f6050d1ee0ead6a075be87e114eee0a2207
workwithjames <thisisjamesravi@gmail.com>
2026-08-20 18:35:11 +0400

    Switch dashboard loader to validated CEO module

--- desktop-nav.js
+++ desktop-nav.js
@@ -1,6 +1,6 @@
 (()=>{
   const script=document.createElement('script');
-  script.src='ceo-app.js?v=20260820-1824';
+  script.src='ceo-app-v2.js?v=20260820-1824';
   script.defer=true;
   document.head.appendChild(script);
 })();
