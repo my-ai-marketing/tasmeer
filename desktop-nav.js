523e6d5cfb26ffe0ef3077958e93bcf42dbc1c56
workwithjames <thisisjamesravi@gmail.com>
2026-08-20 19:09:08 +0400

    Load hardened 16-section presentation flow

--- desktop-nav.js
+++ desktop-nav.js
@@ -9,7 +9,7 @@
   script.defer=true;
   script.onload=()=>{
     const flow=document.createElement('script');
-    flow.src='presentation-flow.js?v=20260820-1849';
+    flow.src='presentation-flow-v2.js?v=20260820-1912';
     flow.defer=true;
     document.head.appendChild(flow);
   };
