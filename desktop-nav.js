a7aff1d3af4ec7473ef57f426b17507c8d880cb7
workwithjames <thisisjamesravi@gmail.com>
2026-08-20 19:39:12 +0400

    Restore stable parallel 12-section loader

--- desktop-nav.js
+++ desktop-nav.js
@@ -2,11 +2,10 @@
   const language=document.createElement('script');
   language.src='presentation-language.js?v=20260820-1839';
   language.defer=true;
-  language.onload=()=>{
-    const script=document.createElement('script');
-    script.src='ceo-app-v2.js?v=20260820-1824';
-    script.defer=true;
-    document.head.appendChild(script);
-  };
   document.head.appendChild(language);
+
+  const script=document.createElement('script');
+  script.src='ceo-app-v2.js?v=20260820-1824';
+  script.defer=true;
+  document.head.appendChild(script);
 })();
