a8cf716d61377d60aa923fc9c9fa4d99ca7af6b3
workwithjames <thisisjamesravi@gmail.com>
2026-08-20 18:41:32 +0400

    Remove CEO wording from rendered presentation

--- desktop-nav.js
+++ desktop-nav.js
@@ -1,4 +1,9 @@
 (()=>{
+  const language=document.createElement('script');
+  language.src='presentation-language.js?v=20260820-1839';
+  language.defer=true;
+  document.head.appendChild(language);
+
   const script=document.createElement('script');
   script.src='ceo-app-v2.js?v=20260820-1824';
   script.defer=true;
